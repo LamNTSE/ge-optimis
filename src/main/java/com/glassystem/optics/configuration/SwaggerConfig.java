86531d9524ad6b5ca9147f708bab98798d50be7f
LamNT <lamntse173420@fpt.edu.vn>
2026-04-19 22:26:57 +0700

    fix

--- src/main/java/com/glassystem/optics/configuration/SwaggerConfig.java
+++ src/main/java/com/glassystem/optics/configuration/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +14,6 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
                 .info(new Info().title("Optics Management API").version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
